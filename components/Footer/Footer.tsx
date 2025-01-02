c86d913b1bcc89939aae30cfd9c3beccb8c8282d
Eimantas <test@tests-MacBook-Pro.local>
2025-01-02 07:59:34 +0200

    Refactored app design

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const Footer = () => {
-  return <div className={styles.wrapper}>Footer</div>;
+  return (
+    <div className={styles.wrapper}>
+      {" "}
+      © Copyright 2025 Balčiūnas.com. All rights reserved.{" "}
+    </div>
+  );
 };
 
 export default Footer;
